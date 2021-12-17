a686694a7cb427e126efc30c5ec51b3c0dd0e819
Cole-Pawliw <81489189+Cole-Pawliw@users.noreply.github.com>
2021-12-17 11:49:50 -0700

    Update project.sql

--- project.sql
+++ project.sql
@@ -5,7 +5,9 @@ USE PROJECT;
 
 DROP TABLE IF EXISTS PERSON;
 CREATE TABLE PERSON (
-	UCID			varchar(9) not null,
+	Username		varchar(100) NOT NULL UNIQUE,
+	Password		varchar(100) NOT NULL,
+	UCID			integer NOT NULL AUTO_INCREMENT,
 	Fname			varchar(25),
 	Minit			char(1),
 	Lname			varchar(25),
@@ -17,22 +19,22 @@ CREATE TABLE PERSON (
 
 DROP TABLE IF EXISTS SCHED;
 CREATE TABLE SCHED (
-	UCID			varchar(9) not null,
+	UCID			varchar(9) NOT NULL,
 	primary key (UCID),
 	foreign key (UCID) references PERSON(UCID) ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS COURSE;
 CREATE TABLE COURSE (
-	Course_id		char(7) not null,
+	Course_id		char(7) NOT NULL,
 	Department		varchar(25),
 	primary key (Course_id)
 );
 
 DROP TABLE IF EXISTS LECTURE_SECTION;
 CREATE TABLE LECTURE_SECTION (
-	Course_id		char(7) not null,
-	Sec_no			integer not null,
+	Course_id		char(7) NOT NULL,
+	Sec_no			integer NOT NULL,
 	Prof_ucid		varchar(9),
 	primary key (Course_id, Sec_no),
 	foreign key (Course_id) references COURSE(Course_id) ON UPDATE CASCADE,
@@ -41,9 +43,9 @@ CREATE TABLE LECTURE_SECTION (
 
 DROP TABLE IF EXISTS LEC_DAY;
 CREATE TABLE LEC_DAY (
-	Course_id		char(7) not null,
-	Sec_no			integer not null,
-	Weekday			varchar(9) not null,
+	Course_id		char(7) NOT NULL,
+	Sec_no			integer NOT NULL,
+	Weekday			varchar(9) NOT NULL,
 	Daytime			char(5),
 	primary key (Course_id, Sec_no, Weekday),
 	foreign key (Course_id, Sec_no) references LECTURE_SECTION(Course_id, Sec_no) ON UPDATE CASCADE
@@ -51,8 +53,8 @@ CREATE TABLE LEC_DAY (
 
 DROP TABLE IF EXISTS LAB_SECTION;
 CREATE TABLE LAB_SECTION (
-	Course_id		char(7) not null,
-	Sec_no			integer not null,
+	Course_id		char(7) NOT NULL,
+	Sec_no			integer NOT NULL,
 	Instructor_ucid	varchar(9),
 	primary key (Course_id, Sec_no),
 	foreign key (Course_id) references COURSE(Course_id) ON UPDATE CASCADE,
@@ -61,9 +63,9 @@ CREATE TABLE LAB_SECTION (
 
 DROP TABLE IF EXISTS LAB_DAY;
 CREATE TABLE LAB_DAY (
-	Course_id		char(7) not null,
-	Sec_no			integer not null,
-	Weekday			varchar(9) not null,
+	Course_id		char(7) NOT NULL,
+	Sec_no			integer NOT NULL,
+	Weekday			varchar(9) NOT NULL,
 	Daytime			char(5),
 	primary key (Course_id, Sec_no, Weekday),
 	foreign key (Course_id, Sec_no) references LECTURE_SECTION(Course_id, Sec_no) ON UPDATE CASCADE
@@ -71,8 +73,8 @@ CREATE TABLE LAB_DAY (
 
 DROP TABLE IF EXISTS TUTORIAL_SECTION;
 CREATE TABLE TUTORIAL_SECTION (
-	Course_id		char(7) not null,
-	Sec_no			integer not null,
+	Course_id		char(7) NOT NULL,
+	Sec_no			integer NOT NULL,
 	Instructor_ucid	varchar(9),
 	primary key (Course_id, Sec_no),
 	foreign key (Course_id) references COURSE(Course_id) ON UPDATE CASCADE,
@@ -81,9 +83,9 @@ CREATE TABLE TUTORIAL_SECTION (
 
 DROP TABLE IF EXISTS TUT_DAY;
 CREATE TABLE TUT_DAY (
-	Course_id		char(7) not null,
-	Sec_no			integer not null,
-	Weekday			varchar(9) not null,
+	Course_id		char(7) NOT NULL,
+	Sec_no			integer NOT NULL,
+	Weekday			varchar(9) NOT NULL,
 	Daytime			char(5),
 	primary key (Course_id, Sec_no, Weekday),
 	foreign key (Course_id, Sec_no) references LECTURE_SECTION(Course_id, Sec_no) ON UPDATE CASCADE
@@ -91,8 +93,8 @@ CREATE TABLE TUT_DAY (
 
 DROP TABLE IF EXISTS FRIENDS;
 CREATE TABLE FRIENDS (
-	Friend_one		varchar(9) not null,
-	Friend_two		varchar(9) not null,
+	Friend_one		varchar(9) NOT NULL,
+	Friend_two		varchar(9) NOT NULL,
 	primary key (Friend_one, Friend_two),
 	foreign key (Friend_one) references PERSON(UCID) ON UPDATE CASCADE,
 	foreign key (Friend_two) references PERSON(UCID) ON UPDATE CASCADE
@@ -100,8 +102,8 @@ CREATE TABLE FRIENDS (
 
 DROP TABLE IF EXISTS SCHED_COURSE;
 CREATE TABLE SCHED_COURSE (
-	UCID			varchar(9) not null,
-	Course_id		char(7) not null,
+	UCID			varchar(9) NOT NULL,
+	Course_id		char(7) NOT NULL,
 	primary key (UCID, Course_id),
 	foreign key (UCID) references PERSON(UCID) ON UPDATE CASCADE,
 	foreign key (Course_id) references COURSE(Course_id) ON UPDATE CASCADE
@@ -109,8 +111,8 @@ CREATE TABLE SCHED_COURSE (
 
 DROP TABLE IF EXISTS SCHED_LEC;
 CREATE TABLE SCHED_LEC (
-	UCID			varchar(9) not null,
-	Lec_sec			integer not null,
+	UCID			varchar(9) NOT NULL,
+	Lec_sec			integer NOT NULL,
 	primary key (UCID, Lec_sec),
 	foreign key (UCID) references PERSON(UCID) ON UPDATE CASCADE,
 	foreign key (Lec_sec) references LECTURE_SECTION(Sec_no) ON UPDATE CASCADE
@@ -118,8 +120,8 @@ CREATE TABLE SCHED_LEC (
 
 DROP TABLE IF EXISTS SCHED_LAB;
 CREATE TABLE SCHED_LAB (
-	UCID			varchar(9) not null,
-	Lab_sec			integer not null,
+	UCID			varchar(9) NOT NULL,
+	Lab_sec			integer NOT NULL,
 	primary key (UCID, Lab_sec),
 	foreign key (UCID) references PERSON(UCID) ON UPDATE CASCADE,
 	foreign key (Lab_sec) references LAB_SECTION(Sec_no) ON UPDATE CASCADE
@@ -127,8 +129,8 @@ CREATE TABLE SCHED_LAB (
 
 DROP TABLE IF EXISTS SCHED_TUT;
 CREATE TABLE SCHED_TUT (
-	UCID			varchar(9) not null,
-	Tut_sec			integer not null,
+	UCID			varchar(9) NOT NULL,
+	Tut_sec			integer NOT NULL,
 	primary key (UCID, Tut_sec),
 	foreign key (UCID) references PERSON(UCID) ON UPDATE CASCADE,
 	foreign key (tut_sec) references TUTORIAL_SECTION(Sec_no) ON UPDATE CASCADE
